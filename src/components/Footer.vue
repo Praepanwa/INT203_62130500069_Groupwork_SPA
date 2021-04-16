4acd362ab91523ac678885d0c354416bf43b90b3
Phaeng <praepanwa.phaeng@mail.kmutt.ac.th>
2021-04-16 21:50:55 +0700

    [UPDATE] 62130500069 comment system

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="footer"  >
+  <div class="footer">
       <p>Drop by Dough open every day</p>
       <p>order online via line man, grab, gojek and line official @DROPBYDOUGH</p>
       <p>@2021 by 2000 meters Co.,Ltd. All Right Reserved.</p>
--- src/main.js
+++ src/main.js
@@ -3,5 +3,7 @@ import App from './App.vue'
 import './assets/tailwind.css'
 import router from './router'
 import BaseCard from './components/BaseCard.vue'
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
 
 createApp(App).use(router).component('base-card',BaseCard).mount('#app')
